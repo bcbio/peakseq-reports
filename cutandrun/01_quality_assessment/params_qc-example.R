ceb4bfa57d5f5021f3b2d1980b94592f691c5a13
Alex Bartlett <bartlettalexandra@gmail.com>
2025-08-21 13:15:16 -0400

    cutandrun qc template using test data

---
+++ cutandrun/01_quality_assessment/params_qc-example.R
@@ -0,0 +1,12 @@
+# info params
+# Example data
+
+# this is the samplesheet used to run nf-core, with additional columns containing covariates of interest
+coldata_fn='https://raw.githubusercontent.com/bcbio/bcbioR-test-data/refs/heads/main/cutandrun/cutandrun_test_samplesheet.csv'
+
+# This folder is in the output directory inside multiqc folder
+multiqc_data_dir='https://raw.githubusercontent.com/bcbio/bcbioR-test-data/refs/heads/main/cutandrun/04_reporting/multiqc/multiqc_data/'
+
+# This folder is in the output directory
+# peaks_dir = 'https://raw.githubusercontent.com/bcbio/bcbioR-test-data/refs/heads/main/cutandrun/03_peak_calling/04_called_peaks/macs2/'
+peaks_dir = '.'
---
+++ cutandrun/01_quality_assessment/params_qc.R
@@ -0,0 +1,8 @@
+# info params
+
+
+coldata_fn='/path/to/nf-core/samplesheet.csv'
+# This folder is in the nf-core output directory inside multiqc folder
+multiqc_data_dir='/path/to/nf-core/output/04_reporting/multiqc/multiqc_data/'
+# This folder is in the nf-core output directory, maybe is broadPeak instead of narrowPeak
+peaks_dir = '/path/to/nf-core/output/03_peak_calling/04_called_peaks/macs2/'
